861a959a586ad6f58ca016f969bb460de64cc22f
Francesco Guastamacchia <52241292+guastamacchia@users.noreply.github.com>
2026-01-28 21:25:45 +0100

    Fix Keycloak admin client auth grant

--- sanitech-svc/svc-directory/src/main/java/it/sanitech/directory/integrations/keycloak/KeycloakAdminProperties.java
+++ sanitech-svc/svc-directory/src/main/java/it/sanitech/directory/integrations/keycloak/KeycloakAdminProperties.java
@@ -2,6 +2,7 @@ package it.sanitech.directory.integrations.keycloak;
 
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
@@ -10,6 +11,8 @@ public record KeycloakAdminProperties(
         @NotBlank String serverUrl,
         @NotBlank String realm,
         @NotBlank String clientId,
-        @NotBlank String clientSecret,
+        @Nullable String clientSecret,
+        @NotBlank String username,
+        @NotBlank String password,
         @NotBlank String tokenPath
 ) {}
