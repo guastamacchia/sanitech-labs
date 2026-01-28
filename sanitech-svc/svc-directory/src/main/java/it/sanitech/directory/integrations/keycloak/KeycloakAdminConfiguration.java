ef703ba081d86c11ae57b08057a64623c62b8686
Francesco Guastamacchia <52241292+guastamacchia@users.noreply.github.com>
2026-01-28 20:35:12 +0100

    Add admin credentials to Keycloak config

--- sanitech-svc/svc-directory/src/main/java/it/sanitech/directory/integrations/keycloak/KeycloakAdminConfiguration.java
+++ sanitech-svc/svc-directory/src/main/java/it/sanitech/directory/integrations/keycloak/KeycloakAdminConfiguration.java
@@ -1,16 +1,26 @@
 package it.sanitech.directory.integrations.keycloak;
 
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableConfigurationProperties(KeycloakAdminProperties.class)
 public class KeycloakAdminConfiguration {
 
-    @Bean
-    public RestTemplate keycloakRestTemplate() {
-        return new RestTemplate();
+    @Bean(destroyMethod = "close")
+    public Keycloak keycloak(KeycloakAdminProperties properties) {
+        return KeycloakBuilder.builder()
+                .serverUrl(properties.serverUrl())
+                .realm(properties.realm())
+                .clientId(properties.clientId())
+                .clientSecret(properties.clientSecret())
+                .username(properties.username())
+                .password(properties.password())
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .build();
     }
 }
