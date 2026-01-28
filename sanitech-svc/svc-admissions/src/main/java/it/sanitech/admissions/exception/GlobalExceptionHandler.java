4a25a2df6ac42dffe70074fc636e117a00cdf231
Francesco Guastamacchia <52241292+guastamacchia@users.noreply.github.com>
2026-01-28 09:29:24 +0100

    Handle optional S3 bucket init in local

--- sanitech-svc/svc-admissions/src/main/java/it/sanitech/admissions/exception/GlobalExceptionHandler.java
+++ sanitech-svc/svc-admissions/src/main/java/it/sanitech/admissions/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import it.sanitech.commons.exception.ProblemDetails;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * Gestore delle eccezioni specifiche per i ricoveri.
  */
 @RestControllerAdvice
+@Component("admissionsGlobalExceptionHandler")
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoBedAvailableException.class)
